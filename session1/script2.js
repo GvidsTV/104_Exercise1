2bd851e0a06e8cafcacafa3c0ebc7341645c38b5
GvidsTV <62351613+GvidsTV@users.noreply.github.com>
2020-05-02 10:11:55 -0700

    Update script2.js

--- session1/script2.js
+++ session1/script2.js
@@ -1,198 +1,82 @@
+console.log("Executed");
+
 /* 
+// loop
 
-console.time('Hi')
-console.log('Hello')
-console.error('Error....')
-console.warn('Warning....')
-onsole.log('Hello')
-console.error('Error....')
-console.warn('Warning....')
-onsole.log('Hello')
-console.error('Error....')
-console.warn('Warning....')
-onsole.log('Hello')
-console.error('Error....')
-console.warn('Warning....')
-console.time('Hi')
+for(let i=0; i<10;i++){
+    // console.log("Numbers"+i ); old way
+    console.log(`Number: ${i}`);
 
-*/
+}
 
-/*
-// varibles var let const
+const cart=['product1','product2','product3','product4'];
 
-//var is the old way
+console.table(cart);
+// travel the array
 
-var studentName="Daniel"; // camelCase
-console.log(studentName);
 
-var student_name="Rene"; // under_score
-console.log(student_name);
+// j=postion (idex)
+// intelligent way
+for(let j=0; j<cart.length; j++){
+    console.log(`Products: ${cart[j]}`);
 
-//let is new
+}
 
-let clientName = "Gareth"
-clientName = "Joel";
+// :(
+    console.log("List of products:" );
+    console.log(`Products: ${cart[0]}`);
+    console.log(`Products: ${cart[1]}`);
+    console.log(`Products: ${cart[2]}`);
+    console.log(`Products: ${cart[3]}`);
+    console.log(`Products: ${cart[4]}`); 
 
-//const is useful for selecting parts of html
+    /*/
 
-const client = "Christian";
-*/
+const logged = true;    
 
-// Strings 
+if(logged == true){
+    console.log('Welcome to the system');
 
-/* let band= "Metallica", song="Enter Sandman";
+} else{
+    console.log('Register into the system');
 
-let name;
+}
 
-name = band+ ' ' + song // concat the strings
-console.log(name);
+// === means the same value and smae datatype
 
-name = name.concat(" ","is great !");
+const age=100;
+if(age===100){
+    console.log("You are a superhero");
 
-console.log(name);
+} else{
+    console.log("You are a mortal");
 
-name= name.toUpperCase();
-console.log(name);
+} 
 
-name= name.toLowerCase();
-console.log(name);
+let cash=600;
+let totalCart=1000;
+let card=false;
 
-console.log(`${band} ${song}`); // template string
- */
+if(cash>totalCart){
+    console.log("Sucessful");
 
- /*
-let name;
-let activity= "I'm learning JavaScript in FSDI";
+}else if(card==true){
+    console.log('The client pay with a debit/credit card');
 
-name=activity.includes('JavaScript');
-// name=activity.split(' '); //csv
+}else{
+    console.log('Failed');
+}
 
-// let interest= "Read, walk, listen to music, programing learning"
+let hour=1;
+//&& means AND || means OR
+if(hour > 0 && hour<= 12){
+    console.log('Good morning');
 
-// name=interest.split(',');
-console.log(activity);
+} else if(hour >12 && hour<=18){
+    console.log('Good Afternoon');
 
-//let email="g.butler85gmail.com";
-//name=email.includes('@');
-console.log(activity.repeat(5));
+} else if(hour >18 && hour<=24);
+    console.log('Good Night');
 
-
-
-
-let num1=5;
-let num2=6;
-
-let result;
-result=num1+num2;
-result=result*num1;
-
-console.log(num1+num2);
-console.log(num1-num2);
-console.log(num1*num2);
-console.log(num1/num2);
-
-result= Math.round(2.55);
-result=Math.floor(2.99);
-result=Math.ceil(2.1);
-
-result=Math.sqrt(144);
-result=Math.min(10,2,5,8,7,6);
-result=Math.max(10,2,5,8,7,6);
-
-
-console.log(result);
-
-
-
-let num1=50,
-    num2="10",
-    num3="two",
-    num4="20.20";
-
- let data;
- 
- console.log(num1+Number(num2));
-
- // <h2> ${nameVariable} 
- console.log(num1+parseFloat(num4));
-
- console.log(Number(num3));
-
- data= Number("20.456");
-
- data=Number("Hellp");
-  console.log(data);
-
-
-  let data= 9987437456901234;
-  console.log(data);
-  data = String(data);
-
-  console.log(data);
-  console.log(data.length);
-
-
-  
-
-  const name="Dominique";
-  
-  console.log(typeof name);
-
-  const age=32;
-
-  console.log(typeof age);
-
-  const student= true;
-  
-  console.log(typeof student);
-
-  let languages = [ 'HTML', 'CSS', 'JS', 'Python']; //array
-
-
-  console.log(typeof languages);
-
-  let person={
-      name:'Christopher',
-      country: 'USA'
-
-      
-  }
-     console.log(person)
-     */
-
-     const numbers = [10,20,30,40,50,60]
-     console.table(numbers);
-
-     const months=new Array('Jan','Feb','March','April');
-     console.log(months);
-     console.log(months[2]);
-
-     months[4]="May";
-
-     console.table(months);
-
-     months.push("June");
-     months.push("July");
-     console.table(months);
-
-     months.unshift('Month 0');
-
-     console.table(months);
-
-     // delete element
-
-     months.pop();
-     months.shift();
-     console.table(months);
-
-     // delete severl elements
-
-     months.splice(1,2);
-     console.table(months);
-
-     console.table(months.reverse());
-     console.table(months.sort());
-
-
-
+//Exercise: display on console Good afternoon and good night 
 
