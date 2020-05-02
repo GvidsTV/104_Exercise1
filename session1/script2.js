a5b0587ad18af958155783a4a124576041b9e8b0
GvidsTV <62351613+GvidsTV@users.noreply.github.com>
2020-05-01 23:45:31 -0700

    Add files via upload

---
+++ session1/script2.js
@@ -0,0 +1,79 @@
+console.log("Executed");
+
+/* 
+// loop
+
+for(let i=0; i<10;i++){
+    // console.log("Numbers"+i ); old way
+    console.log(`Number: ${i}`);
+
+}
+
+const cart=['product1','product2','product3','product4'];
+
+console.table(cart);
+// travel the array
+
+
+// j=postion (idex)
+// intelligent way
+for(let j=0; j<cart.length; j++){
+    console.log(`Products: ${cart[j]}`);
+
+}
+
+// :(
+    console.log("List of products:" );
+    console.log(`Products: ${cart[0]}`);
+    console.log(`Products: ${cart[1]}`);
+    console.log(`Products: ${cart[2]}`);
+    console.log(`Products: ${cart[3]}`);
+    console.log(`Products: ${cart[4]}`); 
+
+    /*/
+
+if(logged){
+    console.log('Welcome to the system');
+
+} else{
+    console.log('Register into the system');
+
+}
+
+// === means the same value and smae datatype
+
+const age=100;
+if(age===100){
+    console.log("You are a superhero");
+
+} else{
+    console.log("You are a mortal");
+
+} 
+
+let cash=600;
+let totalCart=1000;
+let card=false;
+
+if(cash>totalCart){
+    console.log("Sucessful");
+
+}else if(card==true){
+    console.log('The client pay with a debit/credit card');
+
+}else{
+    console.log('Failed');
+}
+
+let hour=1;
+//&& means AND || means OR
+if(hour > 0 && hour<= 12){
+    console.log('Good morning');
+
+} else if(hour >12 && hour<=18){
+    console.log('Good Afternoon');
+
+} else if(hour >18 && hour<=24);
+    console.log('Good Night');
+
+
